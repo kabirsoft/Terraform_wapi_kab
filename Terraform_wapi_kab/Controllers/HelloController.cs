2ae2d7a1a8353383857ba686386320abeb524e96
mka198-cloud <mka198@uit.no>
2023-05-29 15:38:58 +0200

    First commit

---
+++ Terraform_wapi_kab/Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Terraform_wapi_kab.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Hello terraform";
+        }
+    }
+}
---
+++ main.tf
@@ -0,0 +1,30 @@
+provider "azurerm" {   
+  features {}
+}
+
+
+resource "azurerm_resource_group" "terraform_kab" {
+  name = "terraform_rg_kab"
+  location = "Norway East"
+}
+
+resource "azurerm_container_group" "terraform_cg_kab"{
+  name = "weatherapi_kab"
+  location = azurerm_resource_group.terraform_kab.location
+  resource_group_name = azurerm_resource_group.terraform_kab.name
+  ip_address_type = "Public"
+  dns_name_label = "terraformWapikab"
+  os_type = "Linux"
+
+  container {
+    name = "weatherapikab"
+    image = "kabsoft/terraform_wapi_kab"
+    cpu = "1"
+    memory = "1"
+
+    ports {
+      port = 80
+      protocol = "TCP"
+    }
+  }
+}
